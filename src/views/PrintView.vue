2cf28cb2395e3c0c7d07dfbd1757a074ff9536af
Rusu Ionut <johnrusu@gmail.com>
2025-09-29 14:44:07 +0300

    fix: lazy loading

--- src/views/PrintView.vue
+++ src/views/PrintView.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { computed } from 'vue';
+import { useRouter } from 'vue-router';
 
 // utils
 import { isNilOrEmpty } from '@/utils/';
@@ -9,15 +10,22 @@ import { useQRCodeStore } from '@/stores/qrCodeStore';
 
 // constants
 import { LABELS, QR_CODE_API } from '@/constants';
+
+// store
 const qrCodeStore = useQRCodeStore();
+const router = useRouter();
 
 const qrCodeImgSrc = computed(() =>
   !isNilOrEmpty(qrCodeStore.qrcodeImgSrc) ? `${QR_CODE_API}${qrCodeStore.qrcodeImgSrc}` : '',
 );
+
+if (isNilOrEmpty(qrCodeStore.qrcodeImgSrc)) {
+  router.push({ name: 'home' });
+}
 </script>
 <template>
   <div class="print w-full flex flex-col items-center" v-if="!isNilOrEmpty(qrCodeImgSrc)">
-    <img :src="qrCodeImgSrc" :alt="LABELS.QR_CODE" loading="lazy" />
+    <img :src="qrCodeImgSrc" :alt="LABELS.QR_CODE" loading="lazy" class="qr-code-img" />
     <div class="qr-code-label mt-4">{{ LABELS.QR_CODE_IMG_LABEL }}</div>
   </div>
 </template>
